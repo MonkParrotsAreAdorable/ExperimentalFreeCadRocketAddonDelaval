3226f1bdcb39c3ad4a3333b5d985ecae3e56b426
MonkParrotsAreAdorable <18204087541@163.com>
2026-05-22 09:20:54 +0800

    Add files via upload

---
+++ Rockets/InitGui.py
@@ -0,0 +1,128 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-File-Notice: Part of the Rocket addon.
+
+################################################################################
+#                                                                              #
+#   © 2021 David Carter <dcarter@davidcarter.ca>                               #
+#                                                                              #
+#   This addon is free software: you can redistribute it and/or modify         #
+#   it under the terms of the GNU Lesser General Public License as             #
+#   published by the Free Software Foundation, either version 2.1              #
+#   of the License, or (at your option) any later version.                     #
+#                                                                              #
+#   This addon is distributed in the hope that it will be useful,              #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with this addon. If not, see https://www.gnu.org/licenses    #
+#                                                                              #
+################################################################################
+
+
+
+__author__ = "David Carter"
+__url__ = "https://www.davesrocketshop.com"
+
+import FreeCAD
+import FreeCADGui
+
+class RocketWorkbench ( Workbench ):
+    "Rocket workbench object"
+    Icon = FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/icons/RocketWorkbench.svg"
+    MenuText = "Rocket"
+    ToolTip = "Rocket workbench"
+
+    def _loadFemModule(self):
+        # load the FEM module
+        import Fem
+        import FemGui
+        import femcommands.commands
+        # dummy usage to get flake8 and lgtm quiet
+        False if Fem.__name__ else True
+        False if FemGui.__name__ else True
+        False if femcommands.commands.__name__ else True
+
+    def _loadMaterialsModule(self):
+        # load the Materials module
+        import Materials
+        import MatGui
+        # dummy usage to get flake8 and lgtm quiet
+        False if Materials.__name__ else True
+        False if MatGui.__name__ else True
+
+    def _loadCfDModule(self):
+        # load the CfDOF module
+        print("Importing CfdOF")
+        import CfdOF
+        # dummy usage to get flake8 and lgtm quiet
+        False if CfdOF.__name__ else True
+
+    def registerWidgets(self) -> None:
+        # These imports don't seem to be available until now
+        from PySide.QtUiTools import QUiLoader
+        from Ui.Widgets.RocketQuantityField import RocketQuantityField
+        loader = QUiLoader()
+        loader.registerCustomWidget(RocketQuantityField)
+
+    def Initialize(self):
+        FreeCADGui.addLanguagePath(FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/translations")
+        FreeCADGui.updateLocale()
+        self.registerWidgets()
+
+        # load the module
+        # import FreeCAD
+        import RocketGui
+        import SketcherGui
+
+        translate = FreeCAD.Qt.translate
+
+        self._loadFemModule()
+        self._loadMaterialsModule()
+        try:
+            self._loadCfDModule()
+        except:
+            pass
+        self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['De_Laval_Nozzle'])
+        self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['Rocket_Rocket', 'Rocket_Stage', 'Rocket_ParallelStage', 'Rocket_Pod', 'Rocket_NoseCone', 'Rocket_Transition', 'Rocket_BodyTube', 'Rocket_InnerTube', 'Rocket_Coupler', 'Rocket_EngineBlock',
+                        'Rocket_CenteringRing', 'Rocket_Bulkhead', 'Rocket_Fin', 'Rocket_FinCan', 'Rocket_Ringtail', 'Rocket_LaunchLug', 'Rocket_RailButton', 'Rocket_RailGuide',
+                        ])
+        self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['Separator', 'Rocket_MoveUp', 'Rocket_MoveDown'])
+        self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['Separator', 'Rocket_NewSketch', 'Sketcher_EditSketch', 'Separator', 'Rocket_Calculators'])
+
+        try:
+            self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['Separator', 'Rocket_FinFlutter', 'Rocket_CFDAnalysis', "Rocket_MaterialEditor"])
+        except:
+            self.appendToolbar(translate('Rocket', 'Rocket'),
+                        ['Separator', 'Rocket_FinFlutter', "Rocket_MaterialEditor"])
+
+        self.appendMenu(translate('Rocket', 'Rocket'),
+                        ['Rocket_Rocket', 'Rocket_Stage', 'Rocket_ParallelStage', 'Rocket_Pod', 'Rocket_NoseCone', 'Rocket_Transition', 'Rocket_BodyTube', 'Rocket_InnerTube', 'Rocket_Coupler', 'Rocket_EngineBlock',
+                        'Rocket_CenteringRing', 'Rocket_Bulkhead', 'Rocket_Fin', 'Rocket_FinCan', 'Rocket_Ringtail', 'Rocket_LaunchLug', 'Rocket_RailButton', 'Rocket_RailGuide',
+                        ])
+        self.appendMenu(translate('Rocket', 'Rocket'),
+                        ['Separator'])
+        self.appendMenu([translate("Rocket", "Rocket"),
+                         translate("Rocket", "Calculators")],
+                        ['Rocket_CalcBlackPowder', 'Rocket_CalcParachute', 'Rocket_CalcThrustToWeight', 'Rocket_CalcVentHoles'])
+        try:
+            self.appendMenu([translate("Rocket", "Rocket"),
+                            translate("Rocket", "Analysis")],
+                            ['Rocket_FinFlutter', 'Rocket_CFDAnalysis', "Rocket_MaterialEditor"])
+        except:
+            self.appendMenu([translate("Rocket", "Rocket"),
+                         translate("Rocket", "Analysis")],
+                        ['Rocket_FinFlutter', "Rocket_MaterialEditor"])
+
+    def GetClassName(self):
+        return "Gui::PythonWorkbench"
+
+FreeCADGui.addWorkbench(RocketWorkbench())
+
+FreeCAD.__unit_test__ += ["TestRocketGui"]
---
+++ Rockets/RocketGui.py
@@ -0,0 +1,190 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# SPDX-File-Notice: Part of the Rocket addon.
+
+################################################################################
+#                                                                              #
+#   © 2021 David Carter <dcarter@davidcarter.ca>                               #
+#                                                                              #
+#   This addon is free software: you can redistribute it and/or modify         #
+#   it under the terms of the GNU Lesser General Public License as             #
+#   published by the Free Software Foundation, either version 2.1              #
+#   of the License, or (at your option) any later version.                     #
+#                                                                              #
+#   This addon is distributed in the hope that it will be useful,              #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty                #
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    #
+#   See the GNU Lesser General Public License for more details.                #
+#                                                                              #
+#   You should have received a copy of the GNU Lesser General Public           #
+#   License along with this addon. If not, see https://www.gnu.org/licenses    #
+#                                                                              #
+################################################################################
+
+
+
+__author__ = "David Carter"
+__url__ = "https://www.davesrocketshop.com"
+
+import FreeCAD
+import FreeCADGui
+
+translate = FreeCAD.Qt.translate
+
+from Ui.Commands.Command import Command
+from Ui.Commands.CmdRocket import CmdRocket, CmdToggleRocket
+from Ui.Commands.CmdStage import CmdStage, CmdToggleStage
+from Ui.Commands.CmdParallelStage import CmdParallelStage, CmdToggleParallelStage
+from Ui.Commands.CmdNoseCone import CmdNoseCone
+from Ui.Commands.CmdTransition import CmdTransition
+from Ui.Commands.CmdCenteringRing import CmdCenteringRing
+from Ui.Commands.CmdBodyTube import CmdBodyTube, CmdCoupler, CmdInnerTube, CmdEngineBlock
+from Ui.Commands.CmdPod import CmdPod
+from Ui.Commands.CmdBulkhead import CmdBulkhead
+from Ui.Commands.CmdLaunchGuides import CmdLaunchLug, CmdRailButton, CmdRailGuide, CmdStandOff
+from Ui.Commands.CmdFin import CmdFin
+from Ui.Commands.CmdFinCan import CmdFinCan
+from Ui.Commands.CmdRingtail import CmdRingtail
+from Ui.Commands.CmdParachute import CmdParachute
+from Ui.Commands.CmdEditTree import CmdMoveUp, CmdMoveDown
+# Nozzle Design
+from Ui.Commands.CmdDelavalNozzle import _DeLavalNozzle
+# Calculators
+from Ui.Commands.CmdCalcBlackPowder import CmdCalcBlackPowder
+from Ui.Commands.CmdCalcParachute import CmdCalcParachute
+from Ui.Commands.CmdScaling import CmdScalingPairs, CmdScalingTubes
+from Ui.Commands.CmdCalcThrustToWeight import CmdCalcThrustToWeight
+from Ui.Commands.CmdCalcVentHoles import CmdCalcVentHoles
+
+# Rocket specific sketcher
+from Ui.Commands.CmdSketcher import CmdNewSketch
+
+# Template generators
+from Ui.Commands.CmdParachuteGore import CmdParachuteGore
+
+# Analysis
+from Ui.Commands.CmdFlutterAnalysis import CmdFinFlutter
+from Ui.Commands.CmdFemAnalysis import CmdFemAnalysis
+# try:
+from Ui.Commands.CmdCFDAnalysis import CmdCFDAnalysis
+# except:
+#     pass
+from Ui.Commands.CmdMaterialEditor import CmdMaterialEditor
+
+from Rocket.Constants import FEATURE_BODY_TUBE
+from Rocket.Constants import FEATURE_LAUNCH_LUG, FEATURE_RAIL_BUTTON, FEATURE_RAIL_GUIDE, FEATURE_OFFSET
+
+FreeCADGui.addCommand('Rocket_Rocket', CmdRocket())
+FreeCADGui.addCommand('Rocket_ToggleRocket', CmdToggleRocket())
+FreeCADGui.addCommand('Rocket_Stage', CmdStage())
+FreeCADGui.addCommand('Rocket_ToggleStage', CmdToggleStage())
+FreeCADGui.addCommand('Rocket_ParallelStage', CmdParallelStage())
+FreeCADGui.addCommand('Rocket_ToggleParallelStage', CmdToggleParallelStage())
+
+FreeCADGui.addCommand('Rocket_NoseCone', CmdNoseCone())
+FreeCADGui.addCommand('Rocket_Transition', CmdTransition())
+FreeCADGui.addCommand('Rocket_CenteringRing', CmdCenteringRing())
+FreeCADGui.addCommand('Rocket_Bulkhead', CmdBulkhead())
+FreeCADGui.addCommand('Rocket_Fin', CmdFin())
+FreeCADGui.addCommand('Rocket_FinCan', CmdFinCan())
+FreeCADGui.addCommand('Rocket_Ringtail', CmdRingtail())
+
+FreeCADGui.addCommand('Rocket_BodyTube', CmdBodyTube())
+FreeCADGui.addCommand('Rocket_Coupler', CmdCoupler())
+FreeCADGui.addCommand('Rocket_InnerTube', CmdInnerTube())
+FreeCADGui.addCommand('Rocket_EngineBlock', CmdEngineBlock())
+
+FreeCADGui.addCommand('Rocket_Pod', CmdPod())
+
+FreeCADGui.addCommand('Rocket_LaunchLug', CmdLaunchLug())
+FreeCADGui.addCommand('Rocket_RailButton', CmdRailButton())
+FreeCADGui.addCommand('Rocket_RailGuide', CmdRailGuide())
+FreeCADGui.addCommand('Rocket_Standoff', CmdStandOff())
+
+FreeCADGui.addCommand('Rocket_Parachute', CmdParachute())
+
+FreeCADGui.addCommand('Rocket_CalcBlackPowder', CmdCalcBlackPowder())
+FreeCADGui.addCommand('Rocket_CalcParachute', CmdCalcParachute())
+FreeCADGui.addCommand('Rocket_CalcThrustToWeight', CmdCalcThrustToWeight())
+FreeCADGui.addCommand('Rocket_CalcVentHoles', CmdCalcVentHoles())
+
+FreeCADGui.addCommand('Rocket_ScalingPairs', CmdScalingPairs())
+FreeCADGui.addCommand('Rocket_ScalingTubes', CmdScalingTubes())
+
+FreeCADGui.addCommand('Rocket_MoveUp', CmdMoveUp())
+FreeCADGui.addCommand('Rocket_MoveDown', CmdMoveDown())
+
+FreeCADGui.addCommand('Rocket_NewSketch', CmdNewSketch())
+
+FreeCADGui.addCommand('Rocket_ParachuteGore', CmdParachuteGore())
+
+FreeCADGui.addCommand('Rocket_FinFlutter', CmdFinFlutter())
+FreeCADGui.addCommand('Rocket_FemAnalysis', CmdFemAnalysis())
+FreeCADGui.addCommand('De_Laval_Nozzle', _DeLavalNozzle()) # Add the Delaval Nozzle Design Command
+try:
+    FreeCADGui.addCommand('Rocket_CFDAnalysis', CmdCFDAnalysis())
+except:
+    pass
+FreeCADGui.addCommand('Rocket_MaterialEditor', CmdMaterialEditor())
+
+class _CalculatorGroupCommand:
+
+    def GetCommands(self):
+        return tuple(['Rocket_CalcBlackPowder', 'Rocket_CalcParachute', 'Rocket_CalcThrustToWeight', 'Rocket_CalcVentHoles'])
+    def GetResources(self):
+        return {
+            'MenuText': translate('Rocket', 'Calculators'),
+            'ToolTip': translate('Rocket', 'Calculators'),
+            'Pixmap': FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/icons/Rocket_Calculator.svg"
+        }
+    def IsActive(self):
+        # Always available, even without active document
+        return True
+
+class _ScalingGroupCommand:
+
+    def GetCommands(self):
+        return tuple(['Rocket_ScalingTubes', 'Rocket_ScalingPairs'])
+    def GetResources(self):
+        return {
+            'MenuText': translate('Rocket', 'Scaling Tools'),
+            'ToolTip': translate('Rocket', 'Scaling Tools'),
+            'Pixmap': FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/icons/Rocket_Scaling.svg"
+        }
+    def IsActive(self):
+        # Always available, even without active document
+        return True
+
+class _TubeGroupCommand(Command):
+
+    def GetCommands(self):
+        return tuple(['Rocket_BodyTube', 'Rocket_Coupler', 'Rocket_InnerTube'])
+    def GetResources(self):
+        return {
+            'MenuText': translate('Rocket', 'Body Tubes'),
+            'ToolTip': translate('Rocket', 'Body Tubes'),
+            'Pixmap': FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/icons/Rocket_BodyTube.svg"
+        }
+    def IsActive(self):
+        if FreeCAD.ActiveDocument:
+            return self.partEligibleFeature(FEATURE_BODY_TUBE)
+        return False
+
+class _GuidesGroupCommand(Command):
+
+    def GetCommands(self):
+        return tuple(['Rocket_LaunchLug', 'Rocket_RailButton', 'Rocket_RailGuide'])
+    def GetResources(self):
+        return {
+            'MenuText': translate('Rocket', 'Launch Guides'),
+            'ToolTip': translate('Rocket', 'Launch Guides'),
+            'Pixmap': FreeCAD.getUserAppDataDir() + "Mod/Rocket/Resources/icons/Rocket_LaunchLug.svg"
+        }
+    def IsActive(self):
+        if FreeCAD.ActiveDocument:
+            return self.partEligibleFeature([FEATURE_LAUNCH_LUG, FEATURE_RAIL_BUTTON, FEATURE_RAIL_GUIDE, FEATURE_OFFSET])
+        return False
+
+FreeCADGui.addCommand('Rocket_Calculators', _CalculatorGroupCommand())
+FreeCADGui.addCommand('Rocket_Scaling', _ScalingGroupCommand())
+FreeCADGui.addCommand('Rocket_BodyTubes', _TubeGroupCommand())
+FreeCADGui.addCommand('Rocket_LaunchGuides', _GuidesGroupCommand())
